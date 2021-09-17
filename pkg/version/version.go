24ea86853acd056cdc8aa5cea175703f6fa1aa05
mihaibuzgau <mihaibuzgau@gmail.com>
2021-09-17 11:47:40 +0000

    fix build

---
+++ pkg/version/version.go
@@ -0,0 +1,30 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/aserto-dev/aserto-idp/shared/version"
+)
+
+// values set by linker using ldflag -X
+var (
+	ver    string // nolint:gochecknoglobals // set by linker
+	date   string // nolint:gochecknoglobals // set by linker
+	commit string // nolint:gochecknoglobals // set by linker
+)
+
+func getVersion() (string, string, string) {
+	return ver, date, commit
+}
+
+func GetVersionString() string {
+	buildInfo := version.GetBuildInfo(getVersion)
+
+	return fmt.Sprintf("%s g%s %s-%s [%s]",
+		buildInfo.Version,
+		buildInfo.Commit,
+		buildInfo.Os,
+		buildInfo.Arch,
+		buildInfo.Date,
+	)
+}
