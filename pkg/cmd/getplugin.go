1e109ac3e5d7d110e1c45630edd1681787922e2e
oanatmaria <oana@aserto.com>
2021-12-14 15:54:08 +0200

    Change get-plugin to accept name:version

--- pkg/cmd/getplugin.go
+++ pkg/cmd/getplugin.go
@@ -2,14 +2,14 @@ package cmd
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/alecthomas/kong"
 	"github.com/aserto-dev/aserto-idp/pkg/cc"
 )
 
 type GetPluginCmd struct {
-	Name    string `short:"n" help:"The idp plugin name you want to download"`
-	Version string `short:"n" help:"The idp plugin version you want to download"`
+	Name string `short:"n" help:"The idp plugin name and version you want to download. Eg: aserto:x.y.z"`
 }
 
 func (cmd *GetPluginCmd) Run(context *kong.Context, c *cc.CC) error {
@@ -18,16 +18,24 @@ func (cmd *GetPluginCmd) Run(context *kong.Context, c *cc.CC) error {
 		return errors.New("no plugin name was provided")
 	}
 
-	if cmd.Version == "" {
-		cmd.Version = "latest"
+	info := strings.Split(cmd.Name, ":")
+	version := "latest"
+
+	if len(info) > 2 {
+		return errors.New("please provide plugin to download as: plugin-name:version")
+	}
+
+	if len(info) == 2 {
+		version = info[1]
+	} else {
 		c.Ui.Exclamation().Msg("no version was provided; downloading latest...")
 	}
 
-	err := c.Retriever.Download(cmd.Name, cmd.Version)
+	err := c.Retriever.Download(info[0], version)
 	if err != nil {
 		return err
 	}
 
-	c.Ui.Normal().Msgf("Plugin %s %s was successfully downloaded", cmd.Name, cmd.Version)
+	c.Ui.Normal().Msgf("Plugin %s %s was successfully downloaded", info[0], version)
 	return nil
 }
